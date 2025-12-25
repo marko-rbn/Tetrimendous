db050b08b057e81d64b2be1dc18fccbf5b7f75b9
Svitlana Ruban <svitlana.ruban.1@gmail.com>
2025-12-25 18:34:07 -0500

    Square challange

--- js/app.js
+++ js/app.js
@@ -4,4 +4,13 @@ export default function app() {
     const grid = new Grid();
     grid.buildTableGrid(12, 20);
     document.getElementById('cell-10-5').classList.add('red-block');
+    document.getElementById('cell-10-6').classList.add('red-block');
+    document.getElementById('cell-9-5').classList.add('red-block');
+    document.getElementById('cell-9-6').classList.add('red-block');
+
+    document.getElementById('cell-1-12').classList.add('green-block');
+    document.getElementById('cell-2-12').classList.add('green-block');
+    document.getElementById('cell-1-13').classList.add('green-block');
+    document.getElementById('cell-2-13').classList.add('green-block');
+
 }
